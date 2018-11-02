71c774f33418cc1b77b38c7600a71aa067fe2919
Piyush Mittal <m@piyushmittal.com>
2018-11-02 16:58:50 +0700

    Circuit Breaker

---
+++ Reading/src/main/java/com/example/demo/ReadingApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingApplication.class, args);
+	}
+}
--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -4,19 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableOAuth2Client
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
-	
+
 	@Bean
-	public Sampler defaultSampler(){
+	public Sampler defaultSampler() {
 		return Sampler.ALWAYS_SAMPLE;
 	}
 
