5b79fcedcb87c564dbdaad4a34d46b2fb84a2d13
Piyush Mittal <m@piyushmittal.com>
2018-11-07 16:14:26 +0700

    Oauth initial submit

---
+++ In28Min/Oauth2Microservices/src/main/java/com/example/demo/Oauth2MicroservicesApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2MicroservicesApplication.class, args);
+	}
+}
